7b9fa3d7793b62af128fa3e62377f60c31f78b1d
HeberTU <heber.trujillo@glovoapp.com>
2022-06-21 08:48:22 +0200

    [NEW] Adding test for semantic versioning.

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Testing library.
+
+Created on: 21/6/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
---
+++ tests/unit/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Unit testing module.
+
+Created on: 21/6/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
