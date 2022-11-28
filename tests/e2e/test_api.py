0df8d168e68f1a5b77b1be55da26901f2fcd3379
HeberTU <heber.trujillo@glovoapp.com>
2022-11-28 09:01:58 +0100

    feat: Adding helper functions to test api

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -5,3 +5,45 @@ Created on: 20/6/22
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
+import uuid
+
+
+def random_suffix() -> str:
+    """Get a random suffix."""
+    return uuid.uuid4().hex[:6]
+
+
+def random_sku(name: str = "") -> str:
+    """Get a random sku.
+
+    Args:
+        name: name od the article.
+
+    Returns:
+        random_sku: name of the article plus a random suffix.
+    """
+    return f"sku-{name}-{random_suffix()}"
+
+
+def random_batchref(name: str = "") -> str:
+    """Get a random batch reference.
+
+    Args:
+        name: name of the batch reference
+
+    Returns:
+        random_batchref: a randomly batch name generated.
+    """
+    return f"batch-{name}-{random_suffix()}"
+
+
+def random_orderid(name: str = "") -> str:
+    """Get a random order id.
+
+    Args:
+        name: name of the order id
+
+    Returns:
+        random_orderid: a random order id generated.
+    """
+    return f"order-{name}-{random_suffix()}"
